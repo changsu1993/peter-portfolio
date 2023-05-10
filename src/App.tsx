bfab6055a218b4941482be0cd2faab8dfc3b1977
Changsu PARK <61610843+changsu1993@users.noreply.github.com>
2023-05-10 17:27:22 +0900

    feat: add AnimatedRoutes tsx page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 // import SideBar from '@components/SideBar';
 import Splash from '@components/Splash';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Splash />
     </Router>
   );
