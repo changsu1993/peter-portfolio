3c74eccfe8018b5029e3a1fdc42819eb1b783cb0
Changsu PARK <61610843+changsu1993@users.noreply.github.com>
2023-05-10 11:47:53 +0900

    feat: add useAnimation hooks

---
+++ src/hooks/useAnimation.tsx
@@ -0,0 +1,28 @@
+export const useAnimation = () => {
+
+  const hoverAnimation = {
+    hover: {
+      scale: 0.98,
+    }
+  }
+
+  const revealAnimation = {
+    initial: {
+      opacity: 0,
+    },
+
+    animate: {
+      opacity: 1,
+      transition: {
+        duration: 2,
+      }
+    }
+  }
+
+  const transition = {
+    duration: 0.8,
+  }
+
+  return { hoverAnimation, revealAnimation, transition }
+  
+}
