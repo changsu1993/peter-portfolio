03b0fdc2ba97b799306dc59cf31fa199faab6123
Changsu PARK <61610843+changsu1993@users.noreply.github.com>
2023-06-01 14:17:32 +0900

    feat: add Header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="fixed">
+      <audio>
+        <source></source>
+      </audio>
+    </div>
+  );
+};
+
+export default Header;
