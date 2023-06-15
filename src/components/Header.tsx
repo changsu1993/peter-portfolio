870f14c55d2771351bb522bb08c8e3289fc666fd
Changsu PARK <61610843+changsu1993@users.noreply.github.com>
2023-06-15 13:29:34 +0900

    feat: add audio play inside Header components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import Bgm from '../assets/music/bgm.mp3';
 
 const Header = () => {
+  const radioRef = useRef<HTMLAudioElement>(null);
+  const [sound, setSound] = useState(false);
+
+  const startRadio = () => {
+    if (radioRef.current != null) {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      radioRef.current.play();
+    }
+    setSound(true);
+  };
+
+  const pauseRadio = () => {
+    if (radioRef.current != null) {
+      radioRef.current.pause();
+    }
+    setSound(false);
+  };
+
   return (
     <header className="fixed flex top-0 w-[95%] h-[10vh] m-auto justify-between z-[2]">
       <div className="flex items-center bg-[#1b1b1b] rounded-[5px]">
-        <audio>
-          <source src="../assets/music/bgm.mp3" />
+        <audio ref={radioRef} loop>
+          <source src={Bgm} />
         </audio>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="2rem"
-          height="2rem"
-          fill="currentColor"
-          className="bi bi-cloud-haze2 m-[7px] text-[#f50057]"
-          viewBox="0 0 16 16"
-        >
-          <path d="M8.5 3a4.002 4.002 0 0 0-3.8 2.745.5.5 0 1 1-.949-.313 5.002 5.002 0 0 1 9.654.595A3 3 0 0 1 13 12H4.5a.5.5 0 0 1 0-1H13a2 2 0 0 0 .001-4h-.026a.5.5 0 0 1-.5-.445A4 4 0 0 0 8.5 3zM0 7.5A.5.5 0 0 1 .5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm2 2a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-2 4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z" />
-        </svg>
+        {sound ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="2rem"
+            height="2rem"
+            fill="currentColor"
+            className="bi bi-cloud-haze2 m-[7px] text-[#f50057]"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8.5 3a4.002 4.002 0 0 0-3.8 2.745.5.5 0 1 1-.949-.313 5.002 5.002 0 0 1 9.654.595A3 3 0 0 1 13 12H4.5a.5.5 0 0 1 0-1H13a2 2 0 0 0 .001-4h-.026a.5.5 0 0 1-.5-.445A4 4 0 0 0 8.5 3zM0 7.5A.5.5 0 0 1 .5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm2 2a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-2 4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="2rem"
+            height="2rem"
+            fill="currentColor"
+            className="bi bi-cloud-haze2 m-[7px] text-[#08fdd8]"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8.5 3a4.002 4.002 0 0 0-3.8 2.745.5.5 0 1 1-.949-.313 5.002 5.002 0 0 1 9.654.595A3 3 0 0 1 13 12H4.5a.5.5 0 0 1 0-1H13a2 2 0 0 0 .001-4h-.026a.5.5 0 0 1-.5-.445A4 4 0 0 0 8.5 3zM0 7.5A.5.5 0 0 1 .5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm2 2a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-2 4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z" />
+          </svg>
+        )}
         <span className="text-[#fff] mr-[5px]">Sound</span>
         <div className="p-[7px]">
-          <span className="text-[#f50057]">OFF</span>
-          <span className="text-[#08fdd8]">ON</span>
+          {sound ? (
+            <span
+              className="text-[#f50057]"
+              onClick={() => {
+                pauseRadio();
+              }}
+            >
+              OFF
+            </span>
+          ) : (
+            <span
+              className="text-[#08fdd8]"
+              onClick={() => {
+                startRadio();
+              }}
+            >
+              ON
+            </span>
+          )}
         </div>
       </div>
       <button className="flex items-center cursor-pointer">
